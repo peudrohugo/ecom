7f5966300a052017e557a333bd751b13e937ce31
Pedro Hugo <pedrohugo.ov09@gmail.com>
2024-02-23 18:59:48 -0300

    feat: client repository and custom exception

---
+++ ecom-api/src/main/java/com/pedrohugo/ecomapi/client/ClientRepository.java
@@ -0,0 +1,5 @@
+package com.pedrohugo.ecomapi.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, String> { }
---
+++ ecom-api/src/main/java/com/pedrohugo/ecomapi/exception/ClientNotFoundException.java
@@ -0,0 +1,13 @@
+package com.pedrohugo.ecomapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ClientNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    public ClientNotFoundException(String message) {
+        super(message);
+    }
+}
