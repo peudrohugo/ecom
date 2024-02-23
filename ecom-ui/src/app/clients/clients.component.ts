f4f440bc4a0ce028963f2f0228fea21c2b009ec5
Pedro Hugo <pedrohugo.ov09@gmail.com>
2024-02-23 20:26:28 -0300

    feat: click to open dialogs to create and remove client

--- ecom-ui/src/app/clients/clients.component.ts
+++ ecom-ui/src/app/clients/clients.component.ts
@@ -5,6 +5,9 @@ import { Client } from '../shared/entities/client';
 import { ClientsService } from '../shared/services/clients.service';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { NewClientDialogComponent } from './new-client/new-client-dialog.component';
+import { ConfirmDialogComponent } from '../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-clients',
@@ -12,12 +15,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./clients.component.scss'],
 })
 export class ClientsComponent implements AfterViewInit {
-  displayedColumns: string[] = ['cpf', 'name', 'phone', 'email'];
+  displayedColumns: string[] = ['cpf', 'name', 'phone', 'email', 'actions'];
   clients = new MatTableDataSource<Client>([]);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
     private clientsService: ClientsService,
+    private dialog: MatDialog,
     private snackBar: MatSnackBar
   ) {
     this.loadClients();
@@ -44,5 +48,87 @@ export class ClientsComponent implements AfterViewInit {
         );
       },
     });
+  }
+
+  openCreateDialog(): void {
+    const dialogRef = this.dialog.open(NewClientDialogComponent, {
+      disableClose: true,
+      height: '350px',
+      width: '550px',
+      data: {
+        client: {},
+        toCreate: false,
+      },
+    });
+
+    dialogRef
+      .afterClosed()
+      .subscribe((data: { client: Client; toCreate: boolean }) => {
+        if (data) {
+          if (data.toCreate) {
+            this.create(data.client);
+          }
+        }
+      });
+  }
+
+  create(clientObj: Client): void {
+    this.clientsService.createProduct(clientObj).subscribe({
+      next: (data: HttpResponse<Client>) => {
+        if (data.status === 201) {
+          this.snackBar.open('Cliente cadastrado com sucesso!', '', {
+            duration: 2500,
+          });
+          this.loadClients();
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error.message);
+        this.snackBar.open(
+          'Houve um problema ao tentar cadastrar o cliente!',
+          '',
+          {
+            duration: 2500,
+          }
+        );
+      },
+    });
+  }
+
+  delete(id: string): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: 'Remover cliente?',
+        message: 'Tem certeza que deseja excluir esse cliente?',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((toDelete: boolean) => {
+      if (toDelete) {
+        this.clientsService.deleteClient(id).subscribe({
+          next: (data: HttpResponse<any>) => {
+            if (data.status === 200) {
+              this.snackBar.open('Cliente removido com sucesso!', '', {
+                duration: 2500,
+              });
+            }
+            this.loadClients();
+          },
+          error: (error: HttpErrorResponse) => console.error(error.message),
+        });
+      }
+    });
+  }
+
+  formatValue(value: string, type: string): string {
+    let formattedValue: string = '';
+    if (type === 'cpf')
+      formattedValue = value.replace(
+        /(\d{3})(\d{3})(\d{3})(\d{2})/,
+        '$1.$2.$3-$4'
+      );
+    if (type === 'phone')
+      formattedValue = value.replace(/(\d{2})(\d{5})(\d{4})/, '($1)$2-$3');
+    return formattedValue;
   }
 }
--- ecom-ui/src/app/clients/clients.module.ts
+++ ecom-ui/src/app/clients/clients.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ClientsRoutingModule } from './clients-routing.module';
 import { IconWarningModule } from '../components/icon-warning/icon-warning.module';
 
 import { ClientsComponent } from './clients.component';
+import { NewClientDialogComponent } from './new-client/new-client-dialog.component';
+
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [ClientsComponent],
+  declarations: [ClientsComponent, NewClientDialogComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     ClientsRoutingModule,
     MatTableModule,
     MatPaginatorModule,
@@ -23,7 +31,12 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule,
     MatIconModule,
     IconWarningModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
   ],
-  providers: [],
+  providers: [provideNgxMask()],
 })
 export class ClientsModule {}
