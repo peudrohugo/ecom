fb6d4be7422fecc59b9db13edc40a20d56033f4c
Pedro Hugo <pedrohugo.ov09@gmail.com>
2024-02-23 17:48:03 -0300

    feat: confirm-dialog component added

---
+++ ecom-ui/src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject, Input } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+})
+export class ConfirmDialogComponent {
+  @Input() title: string = '';
+  @Input() message: string = '';
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string }
+  ) {
+    this.title = data.title;
+    this.message = data.message;
+  }
+}
