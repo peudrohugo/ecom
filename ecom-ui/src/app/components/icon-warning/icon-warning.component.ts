528909b62b814f957005c2e59eee3c17bdde2bf7
Pedro Hugo <pedrohugo.ov09@gmail.com>
2024-02-23 15:10:20 -0300

    feat: icon-warning component

---
+++ ecom-ui/src/app/components/icon-warning/icon-warning.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-warning',
+  templateUrl: './icon-warning.component.html',
+  styleUrls: ['./icon-warning.component.scss'],
+})
+export class IconWarningComponent {
+  @Input() icon: string = '';
+  @Input() message: string = '';
+}
